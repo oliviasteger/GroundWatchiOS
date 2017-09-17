c102ce748e6bf0d89a28fb818f8c22a904d7d561
InspectorElement <inspectorelement@gmail.com>
2017-09-16 20:17:27 -0400

    changes

--- index.android.js
+++ index.android.js
@@ -15,6 +15,6 @@ import MapView from 'react-native-maps';
 import * as CB from 'cloudboost';
 import PropTypes from 'prop-types';
 var createReactClass = require('create-react-class');
-import GroundWatchiOS from './index.js';
+import GroundWatchiOS from 'App.js';
 
 AppRegistry.registerComponent('GroundWatchiOS', () => GroundWatchiOS);
